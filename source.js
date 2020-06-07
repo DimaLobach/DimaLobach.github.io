5e94b7b25ad65c507008d9de3f65827dadeed6b0
DimaLobach <66557254+DimaLobach@users.noreply.github.com>
2020-06-07 17:34:34 +0300

    Update source.js

--- source.js
+++ source.js
@@ -1,4 +1,20 @@
 // создаем рабочую область
 var stage = acgraph.create('stage-container');
-// рисуем прямоугольник
-var stage.rect(25, 50, 350, 300);
+
+// рисуем рамку
+var frame = stage.rect(25, 50, 350, 300);
+
+// рисуем дом
+var walls = stage.rect(50, 250, 200, 100);
+var roof  = stage.path()
+  .moveTo(50, 250)
+  .lineTo(150, 180)
+  .lineTo(250, 250)
+  .close();
+
+// рисуем человечка
+var head = stage.circle(330, 280, 10);
+var neck = stage.path().moveTo(330, 290).lineTo(330, 300);
+var kilt = stage.triangleUp(330, 320, 20);
+var rightLeg = stage.path().moveTo(320, 330).lineTo(320, 340);
+var leftLeg = stage.path().moveTo(340, 330).lineTo(340, 340);
